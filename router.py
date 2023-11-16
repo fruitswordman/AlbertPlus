5e074e527fde2e28152df9f13dfb3e9920ec6f25
Ashley <ajychen26@gmail.com>
2023-11-16 22:26:24 +0800

    working webpage

--- router.py
+++ router.py
@@ -1,9 +1,13 @@
 # app.py
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 @app.route('/send_message', methods=['POST'])
 def send_message():
