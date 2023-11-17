133184b5d0215d2ef812bdbf23f179e95111de49
Wenbo Lu <103844614+fruitswordman@users.noreply.github.com>
2023-11-17 22:09:45 +0800

    Update course_extractor.py

--- application/course_extractor.py
+++ application/course_extractor.py
@@ -1,44 +1,27 @@
 from openai import OpenAI
+from system_prompt import SYSTEM_PROMPT, compile_response
 import json
 import os
 import time
 
-SYSTEM_PROMPT = """
-{
-  "role_description": "You are an academic pathfinder with a friendly and efficient approach. Your task is to engage in a concise conversation with the user.",
-  "conversation_goals": {
-    "initial_greeting": "Begin with a short, warm greeting to engage the user.",
-    "information_gathering": [
-      "Ask the user about their desired major.",
-      "Inquire about the user's current class standing (freshman, sophomore, junior, or senior).",
-      "Find out the user's future concentration or focus area within their major."
-    ],
-    "message_length_limit": "Ensure each message is under 100 words for a swift and focused conversation."
-  },
-  "return_value_format": {
-    "description": "After obtaining all necessary information, compile and return the data in a structured JSON format.",
-    "example": {
-      "major": "[user's major]",
-      "year": "[user's class standing]",
-      "concentration": "[user's desired concentration]"
-    }
-  }
-}
-
-"""
-
 
 class Coursebot:
     def __init__(self) -> None:
-        # client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
-        client = OpenAI(api_key="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")
+        client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
         self.client = client
+
+        file = client.files.create(
+            file=open("spring2024courses_info_CSCI-SHU_processed.json", "rb"),
+            purpose="assistants",
+        )
 
         self.assistant = self.client.beta.assistants.create(
             name="Academic Pathfinder",
             instructions=SYSTEM_PROMPT,
-            tools=[],
+            tools=[compile_response, {"type": "retrieval"}],
             model="gpt-4-1106-preview",
+            file_ids=[file.id],
         )
         self.thread = self.client.beta.threads.create()
 
@@ -60,12 +43,23 @@ class Coursebot:
             self.run = self.client.beta.threads.runs.retrieve(
                 thread_id=self.thread.id, run_id=self.run.id
             )
+        # while True:
+        #     print(self.run.status)
+        #     time.sleep(0.5)
+        # # while self.run.status == 'completed':
+        # #     self.run.retrieve(
+        # #         thread_id=self.thread.id, run_id=self.run.id
+        # #     )
+        # #     print(self.client.beta.threads.messages.list(
+        # #         thread_id=self.thread.id
+        # #     )
+        # #     .data[0])
 
         print_messages = self.client.beta.threads.messages.list(
             thread_id=self.thread.id
         )
 
-        print(print_messages.data)
+        # print(print_messages.data)
         for message in reversed(print_messages.data):
             output = message.role + " : " + message.content[0].text.value
 
@@ -77,6 +71,6 @@ class Coursebot:
         return output
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     coursebot = Coursebot()
-    coursebot.get_info()
+    print(coursebot.get_info('tell me about tell me about intro to computer and data science what is it about'))
